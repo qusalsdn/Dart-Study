56a525d0c325741b0deb4c2f9be8a7abefce7678
변민우 <yja05092@gmail.com>
2023-03-14 21:32:23 +0900

    Named Parameters
    Named parameters를 이용하면 함수의 파라미터의 순서없이 작성할 수 있고 함수에 전달하는 인자가 무엇을 의미하는지 알 수 있다.

--- main.dart
+++ main.dart
@@ -1,18 +1,25 @@
-void hiHello(String name) {
-  print('hi $name');
+// Named parameters를 이용하면 함수의 파라미터의 순서없이 작성할 수 있고 함수에 전달하는 인자가 무엇을 의미하는지 알 수 있다.
+String sayHello({String name = '익명', int age = 99, String country = '우주'}) {
+  return '안녕! $name, 너는 $age살이고, $country에서 왔구나!';
 }
 
-String sayHello(String name) {
-  return 'Hello $name nice to meet you';
+String hiHello(
+    // required로 선언하면 함수의 파라미터가 반드시 존재해야 한다.
+    {required String name,
+    required int age,
+    required String country}) {
+  return '안녕! $name, 너는 $age살이고, $country에서 왔구나!';
 }
-
-// 함수가 한 줄 짜리 함수일 때 아래처럼 fat arrow syntax를 사용하면 코드를 간소화할 수 있다. 또한 return을 적지 않아도 자동으로 return이 된다.
-String arrowHello(String name) => 'arrowHello $name';
-num plus(num a, num b) => a + b;
 
 void main() {
-  hiHello('minu');
-  print(sayHello('minuuu'));
-  print(arrowHello('minuuuuuuuu'));
-  print(plus(1, 2));
+  print(sayHello(
+    age: 24,
+    country: '대한민국',
+    name: '미누',
+  ));
+  print(hiHello(
+    age: 20,
+    country: '대한민국',
+    name: '미누',
+  ));
 }
