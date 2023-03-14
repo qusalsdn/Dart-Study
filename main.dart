0a3895f320781963d6499960d7df99e68c2c00df
변민우 <yja05092@gmail.com>
2023-03-14 20:34:53 +0900

    Sets
    set과 list의 다른점은 set에 속한 모든 아이템들은 유니크하다.

--- main.dart
+++ main.dart
@@ -1,19 +1,10 @@
 void main() {
-  var player = {
-    'name': 'minu',
-    'xp': 19.99,
-    'skill': false,
-  };
-
-  Map<int, bool> newPlayer = {
-    1: true,
-    2: false,
-    3: true,
-  };
-
-  Map<List<int>, bool> newPlay = {
-    [1, 2, 3, 4, 5]: true,
-  };
-
-  print(newPlay);
+  // set과 list의 다른점은 set에 속한 모든 아이템들은 유니크하다.
+  var numbers = {1, 2, 3, 4, 5};
+  Set<int> number = {1, 2, 3, 4, 5};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  // numbers에 1을 3번이나 추가했지만 결과는 추가되지 않는다.
+  print(numbers);
 }
