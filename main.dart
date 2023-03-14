8c21ee081c8c5a88678e5ee4d3e8eca0fed6eb30
변민우 <yja05092@gmail.com>
2023-03-14 21:59:38 +0900

    QQ Operator
    1. ?? 연산자를 이용하면 왼쪽 값이 null인지 체크해서 null이 아니면 왼쪽 값을 리턴하고 null이면 오른쪽 값을 리턴한다.
    2. ??= 연산자를 이용하면 변수 안에 값이 null일 때를 체크해서 값을 할당해줄 수 있다.

--- main.dart
+++ main.dart
@@ -1,6 +1,14 @@
-String sayHello(String name, int age, [String? country = '대한민국']) =>
-    '안녕! $name, 너는 $age살이고, $country에서 왔구나!';
+String capitalizeName(String? name) =>
+    // ?? 연산자를 이용하면 왼쪽 값이 null인지 체크해서 null이 아니면 왼쪽 값을 리턴하고 null이면 오른쪽 값을 리턴한다.
+    name?.toUpperCase() ?? '알 수 없음...';
 
 void main() {
-  print(sayHello('미누', 24));
+  print(capitalizeName('minu'));
+  print(capitalizeName(null));
+  String? name;
+  // ??= 연산자를 이용하면 변수 안에 값이 null일 때를 체크해서 값을 할당해줄 수 있다.
+  name ??= 'minu';
+  name = null;
+  name ??= 'woo';
+  print(name);
 }
