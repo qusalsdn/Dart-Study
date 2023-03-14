8fb67592cf180a650cb340cdf8690056b7fad4c9
변민우 <yja05092@gmail.com>
2023-03-14 15:40:43 +0900

    The Var Keyword
    1. main함수는 모든 Dart 프로그램의 Entry point이다. main 함수에서 쓴 코드가 호출된다. (만약 main이 없다면 실행이 되지 않음)
    2. dart는 자동으로 세미콜론을 붙여주지 않기 때문에 직접 붙여야 한다. (일부러 세미콜론을 안 쓸 때가 있기 때문)
    3. 함수나 메소드 내부에 지역변수를 선언할 때는 var를 사용하고 class에서 변수나 property를 선언할 때는 타입을 지정해준다.

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
-// main함수는 모든 Dart 프로그램의 Entry point이다. main 함수에서 쓴 코드가 호출된다. (만약 main이 없다면 실행이 되지 않음)
-// dart는 자동으로 세미콜론을 붙여주지 않기 때문에 직접 붙여야 한다. (일부러 세미콜론을 안 쓸 때가 있기 때문)
 void main() {
-  print('hello world');
+  // 함수나 메소드 내부에 지역변수를 선언할 때는 var를 사용하고 class에서 변수나 property를 선언할 때는 타입을 지정해준다.
+  String name = '미누';
+  name = '미누누누누누';
+  var newName = 'minu';
 }
