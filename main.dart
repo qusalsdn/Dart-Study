7b1e1b7cc991d9bef2ed348d493e588107a3042e
변민우 <yja05092@gmail.com>
2023-03-14 21:46:10 +0900

    Optional Positional Parameters
    Dart에서 [] 은 optional, positional parameter를 명시할 때 사용된다.
    name, age는 필수값이고 []를 통해 country를 optional값으로 지정해줄 수 있다.

--- main.dart
+++ main.dart
@@ -1,25 +1,6 @@
-// Named parameters를 이용하면 함수의 파라미터의 순서없이 작성할 수 있고 함수에 전달하는 인자가 무엇을 의미하는지 알 수 있다.
-String sayHello({String name = '익명', int age = 99, String country = '우주'}) {
-  return '안녕! $name, 너는 $age살이고, $country에서 왔구나!';
-}
-
-String hiHello(
-    // required로 선언하면 함수의 파라미터가 반드시 존재해야 한다.
-    {required String name,
-    required int age,
-    required String country}) {
-  return '안녕! $name, 너는 $age살이고, $country에서 왔구나!';
-}
+String sayHello(String name, int age, [String? country = '대한민국']) =>
+    '안녕! $name, 너는 $age살이고, $country에서 왔구나!';
 
 void main() {
-  print(sayHello(
-    age: 24,
-    country: '대한민국',
-    name: '미누',
-  ));
-  print(hiHello(
-    age: 20,
-    country: '대한민국',
-    name: '미누',
-  ));
+  print(sayHello('미누', 24));
 }
