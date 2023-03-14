b87a0429fd2471ab2432d02ba543727132c0550b
변민우 <yja05092@gmail.com>
2023-03-14 16:14:33 +0900

    Late Variables
    초기 데이터 없이 먼저 변수를 생성하고 추후에 데이터를 넣을 때 주로 사용한다. flutter로 data fecthing을 할 때 유용하다. late 변수를 만들고, API에 요청을 보낸 뒤에 API에서 값을 보내주면 그 응답값을 late변수에 넣어 사용할 수 있다.

--- main.dart
+++ main.dart
@@ -1,4 +1,7 @@
 void main() {
-  // var대신 final로 변수를 만들게 되면 이 변수는 수정할 수 없게 된다. (딱 한 번만 설정될 수 있음) 자바스크립트의 const랑 비슷하다.
-  final name = 'minu';
+  // 초기 데이터 없이 먼저 변수를 생성하고 추후에 데이터를 넣을 때 주로 사용한다.
+  // flutter로 data fecthing을 할 때 유용하다.
+  // late 변수를 만들고, API에 요청을 보낸 뒤에 API에서 값을 보내주면 그 응답값을 late변수에 넣어 사용할 수 있다.
+  late final String name;
+  name = 'minu';
 }
