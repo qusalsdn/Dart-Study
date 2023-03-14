7681782a2d1de7924288f1a5284eb9b5c63e8462
변민우 <yja05092@gmail.com>
2023-03-14 20:10:04 +0900

    String Interpolation
    변수 사용하는 방법
    $달러 기호를 붙이고 사용할 변수를 적어주면 된다. 만약 무언가를 계산하고 싶다면 ${ } 형태로 적어주면 된다.

--- main.dart
+++ main.dart
@@ -1,14 +1,7 @@
 void main() {
-  var giveMeFive = true;
-  // list에서 마지막 요소에 ,를 붙이면 아래처럼 각 요소별로 정렬이 된다.
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    // 아래와 같은 문법을 collection if라 부른다. 아래의 if과 같은 역할이지만 좀 더 간결하고 편리하다.
-    if (giveMeFive) 5,
-  ];
-  if (giveMeFive) numbers.add(5);
-  print(numbers);
+  var name = 'minu';
+  var age = 24;
+  // 변수 사용하는 방법은 $달러 기호를 붙이고 사용할 변수를 적어주면 된다. 만약 무언가를 계산하고 싶다면 ${ } 형태로 적어주면 된다.
+  var greeting = "Hello everyone, my name is $name and I'm ${age + 2}";
+  print(greeting);
 }
