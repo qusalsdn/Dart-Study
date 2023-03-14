c91368f3e944f4524cb66bb059a0a69dc0ea5119
변민우 <yja05092@gmail.com>
2023-03-14 20:01:07 +0900

    Lists
    1. list에서 마지막 요소에 ,를 붙이면 아래처럼 각 요소별로 정렬이 된다.
    2. collection if를 사용하면 "존재할 수도 안할 수도 있는 요소를 가지고 올 수 있다."

--- main.dart
+++ main.dart
@@ -1,9 +1,14 @@
 void main() {
-  String name = 'minu';
-  bool alive = true;
-  int age = 24;
-  double money = 69.99;
-  // num 타입은 int가 될 수도 있고 double이 될 수도 있다.
-  num x = 12;
-  x = 1.2;
+  var giveMeFive = true;
+  // list에서 마지막 요소에 ,를 붙이면 아래처럼 각 요소별로 정렬이 된다.
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    // 아래와 같은 문법을 collection if라 부른다. 아래의 if과 같은 역할이지만 좀 더 간결하고 편리하다.
+    if (giveMeFive) 5,
+  ];
+  if (giveMeFive) numbers.add(5);
+  print(numbers);
 }
