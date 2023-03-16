4c0d3d7ce8839e9c7374a3b0de6fb57b5f36791d
변민우 <yja05092@gmail.com>
2023-03-16 16:27:31 +0900

    Inheritance
    super를 이용해 부모 클래스에 접근할 수 있다.

--- main.dart
+++ main.dart
@@ -1,49 +1,29 @@
-// 추상화 클래스는 다른 클래스들이 직접 구현 해야하는 메소드들을 모아놓은 일종의 "청사진"이라 보면 된다.
-// 추상 클래스에서는 기능을 구현하지 않는다.
-abstract class Human {
-  void walk();
+class Human {
+  final String name;
+  Human({required this.name});
+  void sayHello() {
+    print('안녕 내 이름은 $name야!');
+  }
 }
 
-enum Team { red, blue }
+enum Team { blue, red }
 
-enum XpLevel { beginner, medium, pro }
-
-// extends를 이용해 상속, 확장을 할 수 있다.
 class Player extends Human {
-  String name;
-  XpLevel xp;
-  Team team;
-
+  final Team team;
   Player({
-    required this.name,
-    required this.xp,
     required this.team,
-  });
+    required String name,
+  }) : super(name: name); // 상속을 하고 super를 이용해 부모 클래스의 생성자를 호출할 수 있다.
 
-  void walk() {
-    print('Im walking');
-  }
-
-  void playerInfo() {
-    print('PlayerInfo: $name $team $xp');
-  }
-}
-
-class Coach extends Human {
-  // 보다시피 추상화 클래스는 강제로 특정 메소드를 구현하도록 한다.
-  void walk() {
-    print('Coach walking');
+  @override
+  void sayHello() {
+    super.sayHello(); // 상속을 하고 super를 이용해 부모 클래스의 함수를 호출할 수 있다.
+    print('내 팀은 $team이야!');
   }
 }
 
 void main() {
-  var minu = Player(name: 'minu', xp: XpLevel.medium, team: Team.blue)
-    // minu.name='woo';
-    // minu.xp=100000;
-    // minu.team='red';
-    // 위에 처럼 객체를 계속 선언하는 대신 ;을 지우고 ..을 이용하여 값을 변경할 수도 있다.
-    ..name = 'woo'
-    ..xp = XpLevel.pro
-    ..team = Team.red
-    ..playerInfo();
+  var player = Player(team: Team.red, name: '미누');
+  player
+      .sayHello(); // Player class가 Human class를 상속받았기 때문에 sayHello 함수에 접근할 수 있다.
 }
