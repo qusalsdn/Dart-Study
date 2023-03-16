cab5e066584d31382684cea5d14c660400106a52
변민우 <yja05092@gmail.com>
2023-03-16 16:42:49 +0900

    Mixins
    1. Mixin은 생성자가 없는 클래스일 경우에 사용한다.
    2. with을 사용하면 다른 클래스의 프로퍼티와 메소드를 긁어온다. 부모 클래스가 되진 않는다. 한마디로 뺏어온다고 생각하면 쉽다.
    3. Mixin의 핵심은 여러 클래스에 재사용이 가능하다는 점이다.

--- main.dart
+++ main.dart
@@ -6,24 +6,35 @@ class Human {
   }
 }
 
-enum Team { blue, red }
+class Strong {
+  final double strenghLevel = 1500.99;
+}
 
-class Player extends Human {
-  final Team team;
-  Player({
-    required this.team,
-    required String name,
-  }) : super(name: name); // 상속을 하고 super를 이용해 부모 클래스의 생성자를 호출할 수 있다.
-
-  @override
-  void sayHello() {
-    super.sayHello(); // 상속을 하고 super를 이용해 부모 클래스의 함수를 호출할 수 있다.
-    print('내 팀은 $team이야!');
+class QuickRunner {
+  void runQuick() {
+    print('달려려려려려려려려!');
   }
 }
 
+class Tall {
+  final double height = 1.99;
+}
+
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
+enum Team { blue, red }
+
+// Mixin은 생성자가 없는 클래스일 경우에 사용한다.
+// with을 사용하면 다른 클래스의 프로퍼티와 메소드를 긁어온다. 부모 클래스가 되진 않는다. 한마디로 뺏어온다고 생각하면 쉽다.
+// Mixin의 핵심은 여러 클래스에 재사용이 가능하다는 점이다.
+class Player with Strong, QuickRunner, Tall {
+  final Team team;
+  Player({required this.team});
+}
+
 void main() {
-  var player = Player(team: Team.red, name: '미누');
-  player
-      .sayHello(); // Player class가 Human class를 상속받았기 때문에 sayHello 함수에 접근할 수 있다.
+  var player = Player(team: Team.blue);
+  player.runQuick();
 }
